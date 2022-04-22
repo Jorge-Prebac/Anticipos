2ab1315945af2ddd39cbe704a0a7bb4cc0f55406
Daniel Fernández <daniel89fg@gmail.com>
2022-04-22 09:01:40 +0200

    Correcciones varias sobre al editar el anticipo.

--- Controller/EditAnticipo.php
+++ Controller/EditAnticipo.php
@@ -1,7 +1,7 @@
-<?php
+<?php
 /**
  * This file is part of Anticipos plugin for FacturaScripts
- * Copyright (C) 2020 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -16,10 +16,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace FacturaScripts\Plugins\Anticipos\Controller;
 
-use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
-use FacturaScripts\Core\Model\User;
+use FacturaScripts\Core\Lib\ExtendedController\BaseView;
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
 /**
@@ -30,16 +30,16 @@ use FacturaScripts\Core\Lib\ExtendedController\EditController;
 class EditAnticipo extends EditController
 {
 
-	/**
-     * 
+    /**
+     *
      * @return string
      */
     public function getModelClassName()
     {
         return 'Anticipo';
     }
-    
-	/**
+
+    /**
      * Returns basic page attributes
      *
      * @return array
@@ -52,67 +52,89 @@ class EditAnticipo extends EditController
         $data['icon'] = 'fas fa-donate';
         return $data;
     }
-	 
-	/**
-     * 
-     * @param string   $viewName
-     * @param EditView $view
+
+    /**
+     *
+     * @param string $viewName
+     * @param BaseView $view
      */
     protected function loadData($viewName, $view)
-	{
+    {
         switch ($viewName) {
 
-			case 'EditAnticipo':
+            case 'EditAnticipo':
                 parent::loadData($viewName, $view);
-
-                if(!$this->views[$viewName]->model->exists()) {
-                    $this->views[$viewName]->model->user = $this->user->nick;
+                $model = $this->views[$viewName]->model;
+                
+                // si es un anticipo nuevo, se le asigna el usuario que lo creó
+                if (false === $model->exists()) {
+                    $model->user = $this->user->nick;
                 }
 
-				if (false == $this->user->admin) {
-					$this->views[$viewName]->disableColumn('customer', false, 'true');
-					$this->views[$viewName]->disableColumn('user', false, 'true');
-					$this->views[$viewName]->disableColumn('project', false, 'true');
-					$this->views[$viewName]->disableColumn('estimation', false, 'true');
-					$this->views[$viewName]->disableColumn('order', false, 'true');
-					$this->views[$viewName]->disableColumn('delivery-note', false, 'true');
-					$this->views[$viewName]->disableColumn('invoice', false, 'true');
+                // valores para el select de la fase
+                $customValues = [
+                    ['value' => 'Usuario', 'title' => 'user'],
+                    ['value' => 'Cliente', 'title' => 'customer'],
+                    ['value' => 'Proyecto', 'title' => 'project'],
+                    ['value' => 'Presupuesto', 'title' => 'estimation'],
+                    ['value' => 'Pedido', 'title' => 'order'],
+                    ['value' => 'Albaran', 'title' => 'delivery-note'],
+                    ['value' => 'Factura', 'title' => 'invoice'],
+                ];
 
-				} elseif (false === class_exists('\\FacturaScripts\\Dinamic\\Controller\\EditProyecto')) {
-					$this->views[$viewName]->disableColumn('project', false, 'true');
-				}
-                if (!empty ($this->views[$viewName]->model->idfactura)) {
-                    $this->views[$viewName]->model->fase = "Factura";
-
-					if (false == $this->user->admin) {
-						$this->views[$viewName]->disableColumn('amount', false, 'true');
-						$this->views[$viewName]->disableColumn('date', false, 'true');
-						$this->views[$viewName]->disableColumn('note', false, 'true');
-						$this->views[$viewName]->disableColumn('phase', false, 'true');
-						$this->views[$viewName]->disableColumn('payment', false, 'true');
-					}
-				} elseif (!empty ($this->views[$viewName]->model->idalbaran)) {
-                    $this->views[$viewName]->model->fase = "Albaran";
-
-                } elseif (!empty ($this->views[$viewName]->model->idpedido)) {
-                    $this->views[$viewName]->model->fase = "Pedido";
-
-                } elseif (!empty ($this->views[$viewName]->model->idpresupuesto)) {
-                    $this->views[$viewName]->model->fase = "Presupuesto";			
-
-                } elseif (!empty ($this->views[$viewName]->model->idproyecto)) {
-                    $this->views[$viewName]->model->fase = "Proyecto";
-
-                } elseif (!empty ($this->views[$viewName]->model->codcliente)) {
-                    $this->views[$viewName]->model->fase = "Cliente";
-
-                } elseif (!empty ($this->views[$viewName]->model->user)) {
-                    $this->views[$viewName]->model->fase = "Usuario";
+                // si no está instalado el plugin Proyectos ocultamos sus columnas
+                if (false === class_exists('\\FacturaScripts\\Dinamic\\Model\\Proyecto')) {
+                    $this->views[$viewName]->disableColumn('project');
+                    $this->views[$viewName]->disableColumn('project-total-amount');
+                } else {
+                    $customValues[] = ['value' => 'Proyecto', 'title' => 'project'];
                 }
 
-				break;
+                // rellenamos el select de la fase
+                $column = $this->views[$viewName]->columnForName('phase');
+                if($column && $column->widget->getType() === 'select') {
+                    $column->widget->setValuesFromArray($customValues, true);
+                }
 
-			default:
+                // si no eres admin, no puedes editar algunas columnas
+                if (false === $this->user->admin) {
+                    $this->views[$viewName]->disableColumn('customer', false, 'true');
+                    $this->views[$viewName]->disableColumn('user', false, 'true');
+                    $this->views[$viewName]->disableColumn('project', false, 'true');
+                    $this->views[$viewName]->disableColumn('estimation', false, 'true');
+                    $this->views[$viewName]->disableColumn('order', false, 'true');
+                    $this->views[$viewName]->disableColumn('delivery-note', false, 'true');
+                    $this->views[$viewName]->disableColumn('invoice', false, 'true');
+                }
+
+                // si el anticipo es de una facutra y no eres admin no puedes editar estos campos
+                if (false === empty($model->idfactura) && false === $this->user->admin) {
+                    $this->views[$viewName]->disableColumn('amount', false, 'true');
+                    $this->views[$viewName]->disableColumn('date', false, 'true');
+                    $this->views[$viewName]->disableColumn('note', false, 'true');
+                    $this->views[$viewName]->disableColumn('phase', false, 'true');
+                    $this->views[$viewName]->disableColumn('payment', false, 'true');
+                }
+
+                if (false === empty($model->idfactura) && false === $model->exists()) {
+                    $model->fase = "Factura";
+                } elseif (false === empty($model->idalbaran) && false === $model->exists()) {
+                    $model->fase = "Albaran";
+                } elseif (false === empty($model->idpedido) && false === $model->exists()) {
+                    $model->fase = "Pedido";
+                } elseif (false === empty($model->idpresupuesto) && false === $model->exists()) {
+                    $model->fase = "Presupuesto";
+                } elseif (false === empty($model->idproyecto) && false === $model->exists()) {
+                    $model->fase = "Proyecto";
+                } elseif (false === empty($model->codcliente) && false === $model->exists()) {
+                    $model->fase = "Cliente";
+                } elseif (false === empty($model->user) && false === $model->exists()) {
+                    $model->fase = "Usuario";
+                }
+
+                break;
+
+            default:
                 parent::loadData($viewName, $view);
                 break;
         }
