a97b300c8928075fca8d2bdbdb92ea4c2d287d5a
Daniel Fernández <daniel89fg@gmail.com>
2022-04-22 08:58:49 +0200

    Tarea #803 - Corregido como mostrar las columnas de totales en el anticipo.

--- Model/Anticipo.php
+++ Model/Anticipo.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Anticipos plugin for FacturaScripts
- * Copyright (C) 2020 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -16,131 +16,170 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace FacturaScripts\Plugins\Anticipos\Model;
 
 use FacturaScripts\Core\App\AppSettings;
 use FacturaScripts\Core\Model\Base;
-use FacturaScripts\Core\Model\TotalModel;
 use FacturaScripts\Core\Model\ReciboCliente;
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
+use FacturaScripts\Dinamic\Model\AlbaranCliente;
+use FacturaScripts\Dinamic\Model\FacturaCliente;
+use FacturaScripts\Dinamic\Model\PedidoCliente;
+use FacturaScripts\Dinamic\Model\PresupuestoCliente;
 
 /**
  * Description of Anticipo
  *
  * @author Jorge-Prebac <info@prebac.com>
- * @autor Athos Online <info@athosonline.com>
+ * @autor Daniel Fernández Giménez <hola@danielfg.es>
  */
 class Anticipo extends Base\ModelClass
 {
     use Base\ModelTrait;
-    
-	/**
-     * 
+
+    /**
+     *
      * @return string
      */
     public $codcliente;
-    
-	/**
-     * 
+
+    /**
+     *
      * @return string
      */
-	public $coddivisa;
-	
-	/**
+    public $coddivisa;
+
+    /**
      *
      * @var integer
      */
     public $id;
-	
-	/**
-     * 
+
+    /**
+     *
      * @return string
      */
     public $fase;
-	
-	/**
+
+    /**
      *
      * @var string
      */
     public $fecha;
-	
-	/**
+
+    /**
      *
      * @var integer
      */
     public $idalbaran;
-	
-	/**
+
+    /**
      *
      * @var integer
      */
     public $idfactura;
-	
-	/**
+
+    /**
      *
      * @var integer
      */
     public $idpedido;
-	
-	/**
+
+    /**
      *
      * @var integer
      */
     public $idpresupuesto;
-	
-	/**
+
+    /**
      *
      * @var integer
      */
     public $idproyecto;
-	
-	/**
+
+    /**
      *
      * @var integer
      */
     public $idrecibo;
-	
-	/**
+
+    /**
      *
      * @var float
      */
     public $importe;
-	
-	/**
-     * 
+
+    /**
+     *
      * @return string
      */
     public $nota;
-	
-	/**
-     * 
+
+    /**
+     *
      * @return string
      */
     public $user;
-	
+
+    public function __get(string $name)
+    {
+        switch ($name) {
+            case 'totaldelivery':
+                $delivery = new AlbaranCliente();
+                $delivery->loadFromCode($this->idalbaran);
+                return $delivery->total;
+
+            case 'totalestimation':
+                $estimation = new PresupuestoCliente();
+                $estimation->loadFromCode($this->idpresupuesto);
+                return $estimation->total;
+
+            case 'totalinvoice':
+                $invoice = new FacturaCliente();
+                $invoice->loadFromCode($this->idfactura);
+                return $invoice->total;
+
+            case 'totalproject':
+                $modelClass = '\\FacturaScripts\\Dinamic\\Model\\Proyecto';
+                if (class_exists($modelClass)) {
+                    $project = new $modelClass();
+                    $project->loadFromCode($this->idalbaran);
+                    return $project->totalventas;
+                }
+                return 0;
+
+            case 'totalorder':
+                $order = new PedidoCliente();
+                $order->loadFromCode($this->idpedido);
+                return $order->total;
+        }
+        return null;
+    }
+
     public function clear()
-	{
+    {
         parent::clear();
-        $this->coddivisa = AppSettings::get('default' , 'coddivisa');
+        $this->coddivisa = AppSettings::get('default', 'coddivisa');
         $this->fecha = \date(self::DATE_STYLE);
         $this->importe = 0;
     }
-    
-	/**
-     * 
+
+    /**
+     *
      * @return string
      */
     public static function primaryColumn(): string
-	{
+    {
         return 'id';
     }
-    
-	/**
-     * 
+
+    /**
+     *
      * @return string
      */
     public static function tableName(): string
-	{
+    {
         return 'anticipos';
     }
 
@@ -150,35 +189,35 @@ class Anticipo extends Base\ModelClass
             $where = [new DataBaseWhere('idfactura', $this->idfactura)];
             $oldReciboModel = new ReciboCliente();
             $oldRecibos = $oldReciboModel->all($where);
-            
+
             $oldRecibo = new ReciboCliente();
             $oldRecibo->loadFromCode('', $where);
 
             $anticipo = new Anticipo();
             $anticipo->loadFromCode($this->id);
-            
+
             $newRecibo = new ReciboCliente();
             $newRecibo->codcliente = $oldRecibos[0]->codcliente;
             $newRecibo->coddivisa = $anticipo->coddivisa;
             $newRecibo->codigofactura = $oldRecibos[0]->codigofactura;
             $newRecibo->codpago = $anticipo->codpago;
-			$newRecibo->fecha = $anticipo->fecha;
-			if (true == $this->toolBox()->appSettings()->get('anticipos', 'pdAnticipos')) {
-				$newRecibo->fechapago = $anticipo->fecha;	
-			}
+            $newRecibo->fecha = $anticipo->fecha;
+            if (true == $this->toolBox()->appSettings()->get('anticipos', 'pdAnticipos')) {
+                $newRecibo->fechapago = $anticipo->fecha;
+            }
             $newRecibo->idempresa = $oldRecibos[0]->idempresa;
             $newRecibo->idfactura = $this->idfactura;
             $newRecibo->importe = $anticipo->importe;
             $newRecibo->nick = $oldRecibos[0]->nick;
             $newRecibo->numero = count($oldRecibos) + 1;
-			$newRecibo->observaciones = $anticipo->nota;
+            $newRecibo->observaciones = $anticipo->nota;
             $newRecibo->pagado = 1;
             $newRecibo->save();
-        
+
             $oldRecibo->importe = $oldRecibo->importe - $anticipo->importe;
             $oldRecibo->save();
         }
 
         return parent::save();
     }
- }
+}
